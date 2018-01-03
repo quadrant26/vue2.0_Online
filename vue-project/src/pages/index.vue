3704a2e8889827eb6f5059a14b4c71691fb6a642
quadrant26 <quadrant26@163.com>
2018-01-03 11:12:28 +0800

    import slideshow and props data

--- vue-project/src/pages/index.vue
+++ vue-project/src/pages/index.vue
@@ -17,17 +17,115 @@
             </div>
             <div class="index-left-block">
                 <h2>最新消息</h2>
+                <ul>
+                  <li v-for="(item, index) in getNewsList">
+                    <a :href="item.url" class="new-item">{{item.title}}</a>
+                  </li>
+                </ul>
             </div>
         </div>
-        <div class="index-right"></div>
+        <div class="index-right">
+          <sliderShow :slides="slides" :inv="invtime"></sliderShow>
+          <div class="index-board-list">
+            <div class="index-board-item" v-for="(item, index) in boardList" :class="[{'line-last' : index % 2 !== 0},  'index-board' + item.id]">
+              <div class="index-board-item-inner">
+                <h2>{{item.title}}</h2>
+                <p>{{item.description}}</p>
+                <div class="index-board-button">
+                  <router-link :to="{path: 'detail/' + item.tokey}" class="button">立即购买</router-link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
 </template>
 
 <script>
+import sliderShow from '../components/slideShow';
+
 export default {
+  components: {
+    sliderShow
+  },
   data() {
     return {
       msg: "test indexpage",
+      "getNewsList": [
+        {
+          "id": 1,
+          "title": "新闻条目1新闻条目1新闻条目1新闻条目1",
+          "url": "http://starcraft.com"
+        },
+        {
+          "id": 2,
+          "title": "新闻条目2新闻条目2新闻条目2新闻条目2",
+          "url": "http://warcraft.com"
+        },
+        {
+          "id": 3,
+          "title": "新闻条3新闻条3新闻条3",
+          "url": "http://overwatch.com"
+        },
+        {
+          "id": 4,
+          "title": "新闻条4广告发布",
+          "url": "http://hearstone.com"
+        }
+      ],
+      invtime: 5000,
+      slides: [
+        {
+          src: require('../assets/slideShow/pic1.jpg'),
+          title: 'xxx1',
+          href: 'detail/analysis'
+        },
+        {
+          src: require('../assets/slideShow/pic2.jpg'),
+          title: 'xxx2',
+          href: 'detail/count'
+        },
+        {
+          src: require('../assets/slideShow/pic3.jpg'),
+          title: 'xxx3',
+          href: 'http://xxx.xxx.com'
+        },
+        {
+          src: require('../assets/slideShow/pic4.jpg'),
+          title: 'xxx4',
+          href: 'detail/forecast'
+        }
+      ],
+      boardList: [
+        {
+          title: '开放产品',
+          description: '开放产品是一款开放产品',
+          id: 'car',
+          toKey: 'analysis',
+          saleout: false
+        },
+        {
+          title: '品牌营销',
+          description: '品牌营销帮助你的产品更好地找到定位',
+          id: 'earth',
+          toKey: 'count',
+          saleout: false
+        },
+        {
+          title: '使命必达',
+          description: '使命必达快速迭代永远保持最前端的速度',
+          id: 'loud',
+          toKey: 'forecast',
+          saleout: true
+        },
+        {
+          title: '勇攀高峰',
+          description: '帮你勇闯高峰，到达事业的顶峰',
+          id: 'hill',
+          toKey: 'publish',
+          saleout: false
+        }
+      ],
       productList: {
         pc: {
           title: "PC产品",
