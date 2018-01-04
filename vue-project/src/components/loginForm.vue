b80c23398b780f03f94214aedae20df450652c8b
quadrant26 <quadrant26@163.com>
2018-01-04 15:20:43 +0800

    create loginForm.vue

---
+++ vue-project/src/components/loginForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+      <div class="login-form">
+          <div class="g-form">
+              <div class="g-form-line">
+                  <span class="g-form-label">用户名：</span>
+                  <div class="g-form-input">
+                      <input v-model="usernameModel" type="text" placeholder="请输入用户名">
+                  </div>
+              </div>
+              <div class="g-form-line">
+                  <span class="g-form-label">密码：</span>
+                  <div class="g-form-input">
+                      <input v-model="passwordModel" type="password" placeholder="请输入密码">
+                  </div>
+              </div>
+              <div class="g-form-line">
+                    <div class="g-form-btn">
+                    <a class="button" @click="onLogin">登录</a>
+                    </div>
+                </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'LoginForm',
+    data (){
+        return {
+            usernameModel: '',
+            passwordModel: ''
+        }
+    },
+    methods: {
+        onLogin (){
+            console.log( this.usernameModel, this.passwordModel);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
